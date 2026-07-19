cbf2695857b47ab90d330c693707b81452ef42a7
craftique-5544 <2craftique@gmail.com>
2026-07-19 14:14:03 +0530

    Update layout.tsx

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -1,34 +1,17 @@
-"use client";
+import './../css/style.css'
 
-import { useEffect } from "react";
-
-import AOS from "aos";
-import "aos/dist/aos.css";
-
-import Header from "@/components/ui/header";
-import Footer from "@/components/ui/footer";
-
-export default function DefaultLayout({
+export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
-  useEffect(() => {
-    AOS.init({
-      once: true,
-      disable: "phone",
-      duration: 700,
-      easing: "ease-out-cubic",
-    });
-  });
-
   return (
-    <>
-      <Header />
-
-      <main className="grow">{children}</main>
-
-      <Footer border={true} />
-    </>
-  );
+    <html lang="en">
+      <body className="font-inter antialiased bg-white text-gray-900 tracking-tight">
+        <div className="flex flex-col min-h-screen overflow-hidden">
+          {children}
+        </div>
+      </body>
+    </html>
+  )
 }
