e4fb634eb6ff2c98139bde08728a685df349f5cb
craftique-5544 <2craftique@gmail.com>
2026-07-19 13:13:11 +0530

    Refactor Home component and update imports

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,22 +1,11 @@
-export const metadata = {
-  title: "Home - Simple",
-  description: "Page description",
-};
-
 import Hero from "@/components/hero-home";
-import BusinessCategories from "@/components/business-categories";
-import FeaturesPlanet from "@/components/features-planet";
-import LargeTestimonial from "@/components/large-testimonial";
-import Cta from "@/components/cta";
+import FeaturesHome from "@/components/features-home";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <BusinessCategories />
-      <FeaturesPlanet />
-      <LargeTestimonial />
-      <Cta />
+      <FeaturesHome />
     </>
   );
 }
